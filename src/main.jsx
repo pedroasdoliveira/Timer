836bc7a81b0e33af33b168d718512fbd763daaa4
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-10-03 14:20:08 -0300

    feat(Main): Providers

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Providers from "./context";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Providers>
+        <App />
+      </Providers>
     </ChakraProvider>
   </React.StrictMode>
 );

0a166b4cee6698b9e86b2876cdba511a742aaec9
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-10-06 14:50:50 -0300

    style(StartButton): implement styles

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,14 +1,24 @@
-import { Button, useColorModeValue } from '@chakra-ui/react'
-import React from 'react'
+import { Button, useColorModeValue } from "@chakra-ui/react";
+import React from "react";
 
-const StartButton = ({startTimer}) => {
-    const color = useColorModeValue("whiteAlpha", "facebook");
+const StartButton = ({ startTimer }) => {
+  const borderColor = useColorModeValue("#30eae1", "#058b92");
+  const color = useColorModeValue("#124f5b", '#76E4F7');
+  const borderHover = useColorModeValue('#234E52', '#4FD1C5');
+  const colorHover = useColorModeValue('#9DECF9', '#00A3C4');
 
   return (
-    <Button variant="outline" size="lg" colorScheme={color} onClick={startTimer}>
-        Iniciar
+    <Button
+      variant="outline"
+      borderColor={borderColor}
+      color={color}
+      size="lg"
+      _hover={{borderColor: borderHover, color: colorHover}}
+      onClick={startTimer}
+    >
+      Iniciar
     </Button>
-  )
-}
+  );
+};
 
-export default StartButton
+export default StartButton;

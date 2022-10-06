1402aa5b9570540de43060b955d5137b94473600
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-10-06 14:51:03 -0300

    style(StopButton): implement styles

--- src/components/StopButton.jsx
+++ src/components/StopButton.jsx
@@ -1,11 +1,21 @@
 import { Button, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 
-const StopButton = ({pauseTimer}) => {
-  const color = useColorModeValue("whiteAlpha", "facebook");
+const StopButton = ({ pauseTimer }) => {
+  const bgButton = useColorModeValue("#f6ae55e1", "#dd6c20e5");
+  const color = useColorModeValue("#f6ae55e1", "#dd6c20e5");
+  const buttonHover = useColorModeValue('#dd6c20', '#f6ae55');
+  const buttonColor = useColorModeValue('#dd6c20', '#f6ae55');
 
   return (
-    <Button variant="outline" size="lg" colorScheme={color} onClick={pauseTimer}>
+    <Button
+      variant="outline"
+      size="lg"
+      borderColor={bgButton}
+      color={color}
+      _hover={{ borderColor: buttonHover, color: buttonColor }}
+      onClick={pauseTimer}
+    >
       Pausar
     </Button>
   );
